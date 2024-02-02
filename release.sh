2cfee0fa93fcef3bbeee1b86f383ab6be4b4f9be
Shou Ya <shouya@users.noreply.github.com>
2024-02-02 21:22:53 +0900

    chore(release): staged release script

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <version>"
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 --stage-{1,2,3} <version>"
     exit 1
 fi
 
@@ -10,27 +10,54 @@ if [[ -n "$(git status --porcelain)" ]]; then
     exit 1
 fi
 
-version=$1
+stage=$1
+version=$2
 
-make IMAGE_HOST=ghcr.io \
-     VERSION=$version \
-     TARGET=x86_64-unknown-linux-musl \
-     PLATFORM=linux/amd64 \
-     push-docker
+stage1() {
+    make IMAGE_HOST=ghcr.io \
+	 VERSION=$version \
+	 TARGET=x86_64-unknown-linux-musl \
+	 PLATFORM=linux/amd64 \
+	 push-docker
+}
 
-make IMAGE_HOST=ghcr.io \
-     VERSION=$version \
-     TARGET=aarch64-unknown-linux-musl \
-     PLATFORM=linux/arm64/v8 \
-     push-docker
+stage2() {
+    # must clean before cross-compiling
+    # because of https://github.com/cross-rs/cross/issues/724
+    cargo clean
+    make IMAGE_HOST=ghcr.io \
+	 VERSION=$version \
+	 TARGET=aarch64-unknown-linux-musl \
+	 PLATFORM=linux/arm64/v8 \
+	 push-docker
 
-make IMAGE_HOST=ghcr.io \
-     VERSION=$version \
-     push-docker-multiarch
+    make IMAGE_HOST=ghcr.io \
+	 VERSION=$version \
+	 push-docker-multiarch
 
-git tag -a "$version" -m "Release $version"
-git push origin "$version"
+}
 
-git cliff -o CHANGELOG.md
-git commit -am "chore: update changelog"
-git push origin HEAD
+stage3() {
+    git tag -a "$version" -m "Release $version"
+    git push origin "$version"
+
+    git cliff -o CHANGELOG.md
+    git commit -am "chore: update changelog"
+    git push origin HEAD
+}
+
+case "$stage" in
+	--stage-1)
+	    stage1
+	;;
+	--stage-2)
+	    stage2
+	;;
+	--stage-3)
+	    stage3
+	;;
+	*)
+	    echo "Invalid stage"
+	    exit 1
+	;;
+esac
