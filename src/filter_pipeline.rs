1d5b6394bfb1b8060cb6fd386537031f48dcfa12
shouya <shouya@users.noreply.github.com>
2024-03-01 23:36:20 +0900

    store configs in filter pipeline

--- src/filter_pipeline.rs
+++ src/filter_pipeline.rs
@@ -1,5 +1,6 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
 
 use crate::{
   feed::Feed,
@@ -15,19 +16,23 @@ pub struct FilterPipelineConfig {
   filters: Vec<FilterConfig>,
 }
 
-#[derive(Clone)]
 pub struct FilterPipeline {
-  filters: Vec<BoxedFilter>,
+  configs: Mutex<Vec<FilterConfig>>,
+  filters: Mutex<Vec<BoxedFilter>>,
 }
 
 impl FilterPipelineConfig {
   pub async fn build(self) -> Result<FilterPipeline> {
     let mut filters = Vec::new();
+    let configs = self.filters.clone();
     for filter_config in self.filters {
       let filter = filter_config.build().await?;
       filters.push(filter);
     }
-    Ok(FilterPipeline { filters })
+
+    let filters = Mutex::new(filters);
+    let configs = Mutex::new(configs);
+    Ok(FilterPipeline { filters, configs })
   }
 }
 
@@ -40,13 +45,14 @@ impl FilterPipeline {
     let limit_filters = context
       .limit_filters()
       .unwrap_or_else(|| self.num_filters());
-    for filter in self.filters.iter().take(limit_filters) {
+    let filters = self.filters.lock().await;
+    for filter in filters.iter().take(limit_filters) {
       feed = filter.run(&mut context, feed).await?;
     }
     Ok(feed)
   }
 
   pub fn num_filters(&self) -> usize {
-    self.filters.len()
+    self.filters.blocking_lock().len()
   }
 }
