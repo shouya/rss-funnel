52f6b95914307f21c743fc6797094df7bff595f2
shouya <526598+shouya@users.noreply.github.com>
2024-03-29 17:37:04 +0900

    ci: nightly image for commits on master (#102)
    This PR adds a GitHub workflow that builds an image for each commit on
    master, which correspond to a merged PR.
    
    Checklist:
    
    - [x] restore the workflow trigger condition to push on master branch
    only

--- release.sh
+++ release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 --stage-{1,2,3} <version>"
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <version>"
+    echo "Example: $0 0.1.3"
     exit 1
 fi
 
@@ -10,54 +11,11 @@ if [[ -n "$(git status --porcelain)" ]]; then
     exit 1
 fi
 
-stage=$1
-version=$2
+version=$1
+git cliff -o CHANGELOG.md
+git commit -am "chore: update changelog"
+git tag -a "$version" -m "Release $version"
 
-stage1() {
-    make IMAGE_HOST=ghcr.io \
-	 VERSION=$version \
-	 TARGET=x86_64-unknown-linux-musl \
-	 PLATFORM=linux/amd64 \
-	 push-docker
-}
+git push origin HEAD && git push origin "$version"
 
-stage2() {
-    # must clean before cross-compiling
-    # because of https://github.com/cross-rs/cross/issues/724
-    cargo clean
-    make IMAGE_HOST=ghcr.io \
-	 VERSION=$version \
-	 TARGET=aarch64-unknown-linux-musl \
-	 PLATFORM=linux/arm64/v8 \
-	 push-docker
-
-    make IMAGE_HOST=ghcr.io \
-	 VERSION=$version \
-	 push-docker-multiarch
-
-}
-
-stage3() {
-    git tag -a "$version" -m "Release $version"
-    git push origin "$version"
-
-    git cliff -o CHANGELOG.md
-    git commit -am "chore: update changelog"
-    git push origin HEAD
-}
-
-case "$stage" in
-	--stage-1)
-	    stage1
-	;;
-	--stage-2)
-	    stage2
-	;;
-	--stage-3)
-	    stage3
-	;;
-	*)
-	    echo "Invalid stage"
-	    exit 1
-	;;
-esac
+make build-and-push-latest
