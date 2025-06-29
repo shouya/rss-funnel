0a99b0b334b9566c32e2ef173d74a50325ebcc8b
shouya <526598+shouya@users.noreply.github.com>
2025-06-29 17:53:27 +0900

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Build a cargo project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, crane, fenix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
+
+        craneLib = crane.mkLib pkgs;
+        commonArgs = with pkgs; {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
+          nativeBuildInputs = [
+            pkg-config
+          ];
+        };
+
+        rss-funnel = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        });
+      in {
+        checks = {
+          inherit rss-funnel;
+        };
+
+        packages = {
+          default = rss-funnel;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = rss-funnel;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+          packages = with pkgs; [ rust-analyzer ];
+        };
+      });
+}
